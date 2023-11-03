1bcd30ba34b1bfb95b3f28ead4a1cbeef8282e8d
AdarshJha <adarsh2005j@gmail.com>
2023-11-03 21:15:04 +0530

    modified project-4

--- Project-4/script.js
+++ Project-4/script.js
@@ -3,7 +3,6 @@ const body = document.querySelector('body')
 
 allButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        console.log(e);
         console.log(e.target);
         if(e.target.id === 'grey'){
             body.style.backgroundColor = e.target.id
@@ -17,6 +16,5 @@ allButtons.forEach((button) => {
         if(e.target.id === 'yellow'){
             body.style.backgroundColor = e.target.id
         }
-        
     })
 })
