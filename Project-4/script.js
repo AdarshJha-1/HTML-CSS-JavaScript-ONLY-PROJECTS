0dd6dcaace990dfb855926a89c13871078c9bd85
AdarshJha <adarsh2005j@gmail.com>
2023-11-03 21:07:53 +0530

    modified project-4

--- Project-4/script.js
+++ Project-4/script.js
@@ -8,7 +8,7 @@ allButtons.forEach((button) => {
         if(e.target.id === 'grey'){
             body.style.backgroundColor = e.target.id
         }
-        if(e.target.id === 'white'){
+        if(e.target.id === 'pink'){
             body.style.backgroundColor = e.target.id
         }
         if(e.target.id === 'blue'){
